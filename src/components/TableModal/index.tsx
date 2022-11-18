a534d09a06a43c33433630f2107d09658950787d
BrunoMonteiro25 <bruno.monteiro.silva25@gmail.com>
2022-11-18 09:10:35 -0300

    Produtos

---
+++ src/components/TableModal/index.tsx
@@ -0,0 +1,11 @@
+import { Modal } from 'react-native'
+import { Text } from '../Text'
+import { Container } from './styles'
+
+export function TableModal() {
+  return (
+    <Modal>
+      <Text>Teste Modal</Text>
+    </Modal>
+  )
+}
