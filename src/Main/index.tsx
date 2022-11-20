37040f9d5fb513820ea521150cd9f181552bd66e
BrunoMonteiro25 <bruno.monteiro.silva25@gmail.com>
2022-11-20 19:37:03 -0300

    Pedido confirmado

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -182,6 +182,7 @@ export function Main() {
               onAdd={handleAddToCart}
               onDecrement={handleDecrementCartItem}
               onConfirmOrder={handleResetOrder}
+              selectedTable={selectedTable}
             />
           )}
         </FooterContainer>
