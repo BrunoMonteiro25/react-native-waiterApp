9ff4d820e38289a0073bd10fb11ca839d10adb21
BrunoMonteiro25 <bruno.monteiro.silva25@gmail.com>
2022-11-20 18:06:41 -0300

    Conexão com a API

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -1,10 +1,15 @@
-import { Category, Icon } from './styles'
-import { categories } from '../../mocks/categories'
+import { CategoryContainer, Icon } from './styles'
+
 import { Text } from '../Text'
 import { FlatList } from 'react-native'
 import { useState } from 'react'
+import { Category } from '../../types/Category'
 
-export function Categories() {
+interface CategoriesProps {
+  categories: Category[]
+}
+
+export function Categories({ categories }: CategoriesProps) {
   const [selectedCategory, setSelectedCategory] = useState('')
 
   function handleSelectCategory(categoryId: string) {
@@ -23,14 +28,14 @@ export function Categories() {
         const isSelected = selectedCategory === category._id
 
         return (
-          <Category onPress={() => handleSelectCategory(category._id)}>
+          <CategoryContainer onPress={() => handleSelectCategory(category._id)}>
             <Icon>
               <Text opacity={isSelected ? 1 : 0.5}>{category.icon}</Text>
             </Icon>
             <Text opacity={isSelected ? 1 : 0.5} size={14} weight="600">
               {category.name}
             </Text>
-          </Category>
+          </CategoryContainer>
         )
       }}
     />
